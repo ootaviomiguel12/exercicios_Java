bf67ad42c61e4fc56d757e7b2285eed5e4970b71
Otavio <martinsotavio980@gmail.com>
2026-08-09 18:02:51 -0300

    Enviando arquivos ao git

---
+++ calculadora_adicao/src/Main.java
@@ -0,0 +1,34 @@
+//Projetos simples de calculadora
+
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+public static void main(String[] args) {
+
+    //calculadora de inteiros
+//    Scanner primeiroValor = new Scanner(System.in);
+//    Scanner segundoValor = new Scanner(System.in);
+//    System.out.println("Digite o primeiro numero:");
+//    int Valor1 = primeiroValor.nextInt();
+//
+//    System.out.println("Digite o segundo numero:");
+//    int Valor2 = segundoValor.nextInt();
+//
+//    int Resultado = Valor1 + Valor2;
+//      System.out.println("A soma e: " + Resultado);
+
+
+    //calculadora de decimais
+//    Scanner primeiroValor = new Scanner(System.in);
+//    Scanner segundoValor = new Scanner(System.in);
+//    System.out.println("Digite o primeiro numero:");
+//    double Valor1 = primeiroValor.nextDouble();
+//
+//    System.out.println("Digite o segundo numero:");
+//    double Valor2 = segundoValor.nextDouble();
+//
+//    double Resultado = Valor1 + Valor2;
+//    System.out.println("A soma e: " + Resultado);
+    }
+}
