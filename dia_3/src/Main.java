340b5eb9ea67bb71295d0219032181a2bf499017
Otávio Miguel Abreu Martins <martinsotavio980@gmail.com>
2026-08-11 00:57:37 -0300

    Delete dia_3 directory

--- dia_3/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.text.NumberFormat;
-
-public class Main {
-    public static void main (String[] args) {
-        NumberFormat real = NumberFormat.getCurrencyInstance();
-        String productValue = real.format(120.90);
-        System.out.println(productValue);
-    }
-}
